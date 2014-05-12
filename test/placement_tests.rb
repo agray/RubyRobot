fee90923de5b15505c000c069333408e0c972812
Andrew Gray <andrew.paul.gray@gmail.com>
2014-05-12 13:14:35 +1000

    test

--- test/placement_tests.rb
+++ test/placement_tests.rb
@@ -20,7 +20,7 @@
 # * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # * THE SOFTWARE.
 # 
-require './test_base.rb'
+require 'test_base.rb'
 require '../app/direction.rb'
 
 class PlacementTests < TestBase
