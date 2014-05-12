5cbe7286ca5bde2dd47725ad52946346c1dfb325
Andrew Gray <andrew.paul.gray@gmail.com>
2014-05-12 13:20:54 +1000

    update requires

--- app/ruby_robot_core/robot.rb
+++ app/ruby_robot_core/robot.rb
@@ -20,7 +20,7 @@
 # * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # * THE SOFTWARE.
 # 
-require '../app/table.rb'
+require './app/table.rb'
 require_relative 'position.rb'
 require_relative 'posture.rb'
 
